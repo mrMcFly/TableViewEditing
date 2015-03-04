7f667dddcedb8de21bf3dabf44b16ee370e8ae18
Alex Sergienko <Sergienko.a1986@gmail.com>
2015-03-04 15:52:48 +0200

    Initial Commit

---
+++ ASTableViewEditingHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASTableViewEditingHW
+//
+//  Created by Alex Sergienko on 04.03.15.
+//  Copyright (c) 2015 Alexandr Sergienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
