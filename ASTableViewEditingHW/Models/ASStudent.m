046f1dd551e57a17d66e95d771f6440d6bc284f7
Alex Sergienko <alexandersergienkoios@gmail.com>
2015-04-15 13:06:37 +0300

    Final 2.0

--- ASTableViewEditingHW/Models/ASStudent.m
+++ ASTableViewEditingHW/Models/ASStudent.m
@@ -8,21 +8,24 @@
 
 #import "ASStudent.h"
 
-static NSString *firstNames [] = {@"Aiden",@"Jackson",@"Ethan",@"Liam",@"Mason",
-    @"Noah",@"Lucas",@"Jacob",@"Jayden",@"Jack",
-    @"Logan",@"Ryan",@"Caleb",@"Benjamin",@"William",
-    @"Michael",@"Alexander",@"Elijah",@"Matthew",@"Dylan",
-    @"James",@"Owen",@"Connor",@"Brayden",@"Carter",@"Landon",
-    @"Joshua",@"Luke",@"Daniel",@"Gabriel",@"Nicholas",@"Nathan"
+static NSString *firstNames [] = {
+    @"Aiden",   @"Jackson",  @"Ethan",  @"Liam",    @"Mason",
+    @"Noah",    @"Lucas",    @"Jacob",  @"Jayden",  @"Jack",
+    @"Logan",   @"Ryan",     @"Caleb",  @"Benjamin",@"William",
+    @"Michael", @"Alexander",@"Elijah", @"Matthew", @"Dylan",
+    @"James",   @"Owen",     @"Connor", @"Brayden", @"Carter",
+    @"Joshua",  @"Luke",     @"Daniel", @"Gabriel", @"Nicholas"
 };
 
 
-static NSString *lastNames [] = {@"Bender",@"Benjamin",@"Bennett",@"Benson",@"Bentley",
-    @"Clayton",@"Clements",@"Clemons",@"Cleveland",@"Flynn",
-    @"Foley",@"Forbes",@"Ford",@"Howell",@"Hubbard",
-    @"Huber",@"Hudson",@"Mann",@"Manning",@"Marks",@"Marquez",
-    @"Sergienko",@"Petrov",@"Bugaenko",@"Petrenko",@"Kroul",
-    @"Jason",@"Nottingem",@"Demitrov",@"Dubenko",@"Reilly"
+static NSString *lastNames [] = {
+    
+    @"Bender",   @"Benjamin",  @"Bennett", @"Benson",   @"Bentley",
+    @"Clayton",  @"Clements",  @"Clemons", @"Cleveland",@"Flynn",
+    @"Foley",    @"Forbes",    @"Ford",    @"Howell",   @"Hubbard",
+    @"Huber",    @"Hudson",    @"Mann",    @"Manning",  @"Marks",
+    @"Sergienko",@"Petrov",    @"Bugaenko",@"Petrenko", @"Kroul",
+    @"Jason",    @"Nottingem", @"Demitrov",@"Dubenko",  @"Reilly"
 };
 
 static int firstNamesAndLastNamesCount = 30;
@@ -35,7 +38,7 @@ static int firstNamesAndLastNamesCount = 30;
     ASStudent *newStudent = [[ASStudent alloc]init];
     
     newStudent.firstName = firstNames [arc4random() % firstNamesAndLastNamesCount];
-    newStudent.lastName = lastNames [arc4random() % firstNamesAndLastNamesCount];
+    newStudent.lastName  = lastNames  [arc4random() % firstNamesAndLastNamesCount];
     newStudent.averageRating = (CGFloat)(arc4random() % 191 + 270) / 100;
     
     return newStudent;
